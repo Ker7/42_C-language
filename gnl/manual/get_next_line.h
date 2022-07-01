c3a8e09a698db1384402c312f8a6e6b53bf5facb
Kert Mõttus <kert.mottus@gmail.com>
2022-07-01 17:28:23 +0300

    gnl manual thing

---
+++ gnl/manual/get_next_line.h
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kmottus <kmottus@student.42wolfsburg.de    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/06/16 09:50:00 by kmottus           #+#    #+#             */
+/*   Updated: 2022/06/16 09:50:00 by kmottus          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+
+#ifndef BUFFER_SIZE
+# define BUFFER_SIZE 32
+#endif
+
+#ifndef FT_GNL_H
+# define FT_GNL_H
+# include <unistd.h>
+# include <stdlib.h>
+# include <stdarg.h>
+
+
+char	*get_next_line(int fd);
+void	ft_bzero(void *s, size_t n);
+void	*ft_calloc(size_t nmemb, size_t size);
+size_t	ft_strlen(char *s);
+
+#endif
