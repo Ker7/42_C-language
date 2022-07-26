19f60fa0daf55e0a9235a7cb51449e48e699c813
Kert Mõttus <kert.mottus@gmail.com>
2022-07-26 21:10:50 +0300

    clean

--- gnl/manual/rebuild-tool.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-gcc -c get_next_line.c get_next_line_utils.c -DBUFFER_SIZE=421
-ar rc get_next_line.a get_next_line.o get_next_line_utils.o
-gcc main.c get_next_line.a -DBUFFER_SIZE=420
-./a.out "smpl_less-than-buffer.txt"
-#&&./a.out "smpl_more-than-buffer.txt"
--- printf/recomp.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-make fclean
-make
-./a.out
